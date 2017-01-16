492382133098beb90fea495baf7aa6ae3319b401
Raghav <raghav.kishan@gmail.com>
2017-01-16 15:35:48 -0800

    Global Alignment with Scoring Matrix

---
+++ q33.py
@@ -0,0 +1,24 @@
+import sys                              # reference: Biopython pairwise2
+from Bio import pairwise2
+from Bio.pairwise2 import format_alignment
+from Bio.SubsMat import MatrixInfo as matrix
+
+matscores = matrix.blosum62
+
+f = open("rosalind_glob.txt",'r')                   #To extract input from the file
+inputs1 = f.read()
+#print inputs
+seq=inputs1.strip().split('>')
+inputv = []
+for s in seq:
+    if(s!=""):
+        strings=s.split()
+        part1=strings[0]
+        part2=''.join(strings[1:])
+        inputv.append(part2)
+
+
+for i in pairwise2.align.globalds(inputv[0], inputv[1], matscores, -5, -5):         # Determining the score
+    print(format_alignment(*i))
+
+
